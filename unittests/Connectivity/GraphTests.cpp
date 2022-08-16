ddca005b33071c1e0a94538461fb2b1fe983d40b
Jason Lau <lau@cs.ucla.edu>
2022-08-16 17:03:20 -0600

    test: add gtest for unittests

---
+++ unittests/Connectivity/GraphTests.cpp
@@ -0,0 +1,18 @@
+//===- GraphTest.cpp - FIRRTL type unit tests -----------------------------===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "phy/Connectivity/Graph.h"
+#include "gtest/gtest.h"
+
+using namespace phy::connectivity;
+
+namespace {
+
+TEST(GraphTests, GraphCompiles) { ASSERT_TRUE(true); }
+
+} // namespace
