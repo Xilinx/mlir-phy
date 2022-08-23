bb3f96be9af8a45dc702956ab9ade4678581a23f
Jason Lau <lau@cs.ucla.edu>
2022-08-23 13:34:20 -0600

    fix(test): mm_2x2 physical decimals

--- unittests/Connectivity/LiteralVectorTests.cpp
+++ unittests/Connectivity/LiteralVectorTests.cpp
@@ -7,21 +7,25 @@
 //===----------------------------------------------------------------------===//
 
 #include "phy/Connectivity/Serialization/LiteralVector.h"
+
+#include <vector>
+
 #include "gtest/gtest.h"
 
 using namespace phy::connectivity;
 
 namespace {
 
-TEST(Resource, Parses) {
+TEST(LiteralVector, Parses) {
   LiteralVector<int> r("1.1.2");
   EXPECT_EQ(r.vec()[0], 1);
   EXPECT_EQ(r.vec()[1], 1);
   EXPECT_EQ(r.vec()[2], 2);
 }
 
-TEST(Resource, Serializes) {
-  LiteralVector<int> r({1, 2, 3});
+TEST(LiteralVector, Serializes) {
+  std::vector<int> v{1, 2, 3};
+  LiteralVector<int> r(v);
   EXPECT_EQ(r.str(), "1.2.3");
 }
 
