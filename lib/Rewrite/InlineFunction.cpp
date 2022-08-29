9a620b168615266b14cf934cb306140fa6f96b97
Jason Lau <lau@cs.ucla.edu>
2022-08-29 10:53:53 -0600

    feat(rewrite): remove dead blocks after inlining

--- lib/Rewrite/InlineFunction.cpp
+++ lib/Rewrite/InlineFunction.cpp
@@ -10,6 +10,8 @@
 
 #include "phy/Dialect/Physical/PhysicalDialect.h"
 
+#include <list>
+
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Transforms/DialectConversion.h"
 
@@ -22,14 +24,37 @@ FunctionInliner::matchAndRewrite(CallOp op, OpAdaptor adaptor,
                                  ConversionPatternRewriter &rewriter) const {
   Inliner inliner(rewriter.getContext());
 
+  // Get function symbol
   auto sym = op.getCallableForCallee().dyn_cast<SymbolRefAttr>();
   if (!sym)
     return failure();
 
+  // Get FuncOp
   auto func = dyn_cast_or_null<func::FuncOp>(
       SymbolTable::lookupNearestSymbolFrom(op, sym));
   if (!func)
     return failure();
 
-  return inlineCall(inliner, op, func, func.getCallableRegion());
+  // Inline the function body to the location of the call operation.
+  auto result = inlineCall(inliner, op, func, func.getCallableRegion());
+
+  if (result.succeeded()) {
+    auto parent_region = op.getOperation()->getParentRegion();
+
+    // Erase the function calling operation
+    rewriter.eraseOp(op);
+
+    // Erase blocks without predecessors after inlining to make sure all blocks
+    // are reachable.  For example, if the inlined function is an infinite loop,
+    // all the blocks following the function call are unreachable.
+    std::list<Block *> dead_blocks;
+    for (auto &block : *parent_region)
+      if (!block.isEntryBlock() && block.hasNoPredecessors())
+        dead_blocks.push_back(&block);
+
+    for (auto block : dead_blocks)
+      rewriter.eraseBlock(block);
+  }
+
+  return result;
 }
